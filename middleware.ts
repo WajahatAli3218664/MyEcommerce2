960b954948a3911f8129bb86694855c2caa12d1e
Wajahat Ali Khan <assim345678@gmail.com>
2024-12-16 00:54:49 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/account(.*)", "/checkout"]);
+// Define routes that need protection
+const isProtectedRoute = createRouteMatcher(["/account", "/checkout"]);
+
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req))
+  // Protect routes
+  if (isProtectedRoute(req)) {
     auth().protect({
       unauthenticatedUrl: process.env.NEXT_PUBLIC_SERVER_URL,
       unauthorizedUrl: process.env.NEXT_PUBLIC_SERVER_URL,
     });
+  }
 });
 
 export const config = {
+  // Always run for API routes and avoid interfering with static files
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
